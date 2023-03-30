cd08643174375cb44abcb6e5f00b9571b9fd53e1
amir <jramir669@gmail.com>
2023-03-30 13:53:39 +0430

    fix style of search box

--- src/pages/home/components/SearchBox.tsx
+++ src/pages/home/components/SearchBox.tsx
@@ -12,14 +12,12 @@ const SearchBox = () => {
         return (
           <div className={"flex w-full justify-center mt-[350px]"}>
             <div
-              className={
-                "border border-4 border-dark-blue rounded-lg flex bg-dark-blue shadow-lg"
-              }
+              className={"rounded-lg flex bg-dark-blue shadow-lg p-4 gap-x-3"}
             >
               <FormikCategoryAutoComplete
                 placeholder={"چه دکتری میخوای ؟"}
                 name={"city"}
-                className={"w-[300px] h-[80px] border-0 rounded-[4rem] "}
+                className={"w-[300px] h-[60px] border-0 rounded-[4rem] "}
                 textClassName={"text-[18px] pr-[4rem] "}
               />
               <FomrikBimehAutoComplete
@@ -28,10 +26,8 @@ const SearchBox = () => {
                 trailingIcon={() => (
                   <NewspaperIcon className={"w-6 text-base-gray "} />
                 )}
-                className={"w-[300px]  h-[80px] !leading-8 "}
-                setInputClassName={() => "h-[80px]"}
-
-                // textClassName={"text-[18px] pr-[4rem] "}
+                className={"w-[300px]  h-[60px] !leading-8 "}
+                setInputClassName={() => "h-[60px]"}
               />{" "}
               <FormikCityAutoComplete
                 name={"city"}
@@ -39,7 +35,7 @@ const SearchBox = () => {
                 trailingIcon={
                   <NewspaperIcon className={"w-6 text-base-gray "} />
                 }
-                className={"!w-[300px]  !h-[80px] !leading-8 "}
+                className={"!w-[300px]  !h-[60px] !leading-8 "}
                 textClassName={"text-[18px] pr-[4rem] "}
               />
               <div
